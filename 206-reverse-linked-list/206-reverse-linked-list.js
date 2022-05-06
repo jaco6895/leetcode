38e3ae61dedd51914d68169a37215bce6be524f8
Jisu <47763550+jaco6895@users.noreply.github.com>
2022-05-06 18:14:25 +0900

    Time: 103 ms (23.66%), Space: 44.1 MB (67.45%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -10,26 +10,28 @@
  * @return {ListNode}
  */
 
+//[1,2,3]
+
 var reverseList = function(head) {
-    if(!head) return head;
+    if(!head) return head; //필수
     
     let node = head; //1
-    let numNext = node.next;
+    let numNext = node.next; //2
     
-    while(numNext){
-       let numNext2 = numNext.next; // null
+    while(numNext){ //2
+       let numNext2 = numNext.next; //3
        
-       // node = numNext; //2 
-       numNext.next = node; //1
+       numNext.next = node; //[3] = 1
         
-       node = numNext;
+       node = numNext; //[1] = 2
         
-       numNext = numNext2;
+       numNext = numNext2; //[2] = 3
         
     }
     
     head.next = null;
     head = node;
+    // head == 1 , node = [2,3,1]
     
     return node;
 };
