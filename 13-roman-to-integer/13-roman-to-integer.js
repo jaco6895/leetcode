ca67b439c520dd3e3424c45c4981d2cfb777fd2a
Jisu <47763550+jaco6895@users.noreply.github.com>
2022-05-25 09:34:14 +0900

    Time: 197 ms (42.01%), Space: 47.5 MB (47.66%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -5,16 +5,18 @@
 var romanToInt = function(s) {
     let result = 0;
     const checkSymbol = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000};
+    const charArr = s.split("");
         
-    s.split("").forEach((symbol,idx)=>{
+    charArr.forEach((symbol,idx)=>{
         let now = checkSymbol[symbol];
-        let next = checkSymbol[s.split("")[idx+1]];
-        console.log(now,next);
+        let next = checkSymbol[charArr[idx+1]];
+        
         if(now >= next || next == undefined){
            result += now ;
           }else{
            result -= now; 
         }
     });
+    
     return result;
 };
